b994e0e4bb89177e1b434fd58f7131cf7a04edd9
Aman Gupta <aamangptaa@gmail.com>
2022-03-02 03:30:27 +0530

    Time: 12 ms (89.72%), Space: 43.2 MB (23.92%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
@@ -10,14 +10,45 @@
 class Solution {
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         List<Integer> list = new ArrayList<>();
-        List<TreeNode> n2rpath = n2r(root, target);
+//         List<TreeNode> n2rpath = n2r(root, target);
         
-        for(int i = 0; i < n2rpath.size(); i++){
-            TreeNode warn = null;
-            if(i != 0) warn = n2rpath.get(i - 1);
-            kLevelDown(n2rpath.get(i), warn, k, i, list);
-        }
+//         for(int i = 0; i < n2rpath.size(); i++){
+//             TreeNode warn = null;
+//             if(i != 0) warn = n2rpath.get(i - 1);
+//             kLevelDown(n2rpath.get(i), warn, k, i, list);
+//         }
+//         return list;
+        helper(root, target, k, list);
         return list;
+    }
+    public int helper(TreeNode node, TreeNode tar, int k,List<Integer> list){
+        if(node == null) return -1;
+        if(node == tar){
+            klevelDown(node, null, k, list);
+            return 1;
+        }
+        
+        int l = helper(node.left, tar, k, list);
+        if(l != -1){
+            klevelDown(node, node.left, k - l, list);
+            return l + 1;
+        }
+        int r = helper(node.right, tar, k, list);
+        if(r != -1){
+            klevelDown(node, node.right, k - r, list);
+            return r + 1;
+        }
+        return -1;
+    }
+    public void klevelDown(TreeNode node, TreeNode warn, int k, List<Integer> list){
+        if(node == null || node == warn || k < 0) return;
+        if(k == 0) {
+            list.add(node.val);
+            return;
+        }
+        
+        klevelDown(node.left, warn, k - 1, list);
+        klevelDown(node.right, warn, k - 1, list);
     }
     public List<TreeNode> n2r(TreeNode node, TreeNode tar){
         List<TreeNode> base = new ArrayList<>();
