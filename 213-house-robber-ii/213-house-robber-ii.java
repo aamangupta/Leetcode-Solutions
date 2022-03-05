c2dbe9e4fff4d8016a671153069d5f09f9a34941
Aman Gupta <aamangptaa@gmail.com>
2022-03-05 22:12:28 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (29.30%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -2,7 +2,6 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         if(n == 1) return nums[0];
-        if(n == 2) return Math.max(nums[0], nums[1]);
         return Math.max(helper(nums, 0, n - 2), helper(nums, 1, n - 1));
     }
     public int helper(int []nums, int i, int j){
