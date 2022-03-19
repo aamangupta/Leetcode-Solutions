b76103c5955c449d32a82a6f26ce77469d2d55b0
Aman Gupta <aamangptaa@gmail.com>
2022-03-20 00:53:19 +0530

    Time: 14 ms (12.52%), Space: 44.3 MB (63.33%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,24 +1,21 @@
 class Solution {
-    public int helper(String w1, String w2, int i, int j, int [][]dp){
-        if(i == w1.length()) return w2.length() - j;
-        if(j == w2.length()) return w1.length() - i;
-        char c1 = w1.charAt(i), c2 = w2.charAt(j);
-        if(dp[i][j] != -1) return dp[i][j];
-        if(c1 == c2){
-            dp[i][j] = helper(w1, w2, i + 1, j + 1, dp);
-        // --- using recursion we are exoloring all the posssiblities
-        }else{
-            dp[i][j] = Math.min(helper(w1, w2, i, j + 1, dp), // insertion
-                            Math.min(helper(w1, w2, i + 1, j, dp), // deletion
-                                     helper(w1, w2, i + 1, j + 1, dp))) + 1; // replacing
-        }
-        return dp[i][j];
-    }
     public int minDistance(String word1, String word2) {
-        int dp[][] = new int[word1.length()][word2.length()];
-        for(int i = 0; i < dp.length; i++){
-            Arrays.fill(dp[i], -1);
+        int n = word1.length(), m = word2.length(), dp[][] = new int[n + 1][m + 1];
+        for(int i = n; i >= 0; i--){
+            for(int j = m; j >= 0; j--){
+                if(i == n){
+                    dp[i][j] = m - j;
+                }else if(j == m){
+                    dp[i][j] = n - i;
+                }else{
+                    if(word1.charAt(i) == word2.charAt(j)){
+                        dp[i][j] = dp[i + 1][j + 1];
+                    }else {
+                        dp[i][j] = 1 + Math.min(dp[i + 1][j], Math.min(dp[i][j + 1], dp[i + 1][j + 1]));
+                    }
+                }
+            }
         }
-        return helper(word1, word2, 0, 0, dp);
+        return dp[0][0];
     }
 }
