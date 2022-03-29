b1336c76197c9da7257aeb0688c3dc024453452e
Aman Gupta <aamangptaa@gmail.com>
2022-03-29 19:05:57 +0530

    Time: 1 ms (84.88%), Space: 41.5 MB (53.65%) - LeetHub

--- 980-unique-paths-iii/980-unique-paths-iii.java
+++ 980-unique-paths-iii/980-unique-paths-iii.java
@@ -1,49 +1,59 @@
 class Solution {
-    int dir[][] = {{-1,0}, {0,-1}, {1,0}, {0,1}};
-    ArrayList<ArrayList<Integer>> paths = new ArrayList<>();
     
-    public void helper(int [][]arr, int si, int sj, boolean[][]vis, ArrayList<Integer> list, int total){
-        int n = arr.length, m = arr[0].length;
-        int box = si * m + sj;
-        
-        if(arr[si][sj] == 2){
-            list.add(box);
-            if(list.size() == total ){
-                paths.add(new ArrayList<>(list));
-            }
-            list.remove(list.size() - 1);
-            return;
-        }
-        
-        vis[si][sj] = true;
-        list.add(box);
-        
-        for(int i = 0; i < 4; i++){
-            int ni = si + dir[i][0];
-            int nj = sj + dir[i][1];
-            if(ni >= 0 && ni < n && nj >= 0 && nj < m && arr[ni][nj] != -1 && vis[ni][nj] == false){
-                helper(arr, ni, nj, vis, list, total);
-            }
-        }    
-        vis[si][sj] = false;
-        list.remove(list.size() - 1);
-    }
-    
-    
+    int count  = 0;
     public int uniquePathsIII(int[][] grid) {
-        int n = grid.length, m = grid[0].length, block = 0, sti = 0, stj = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == -1) block++;
-                else if(grid[i][j] == 1){
-                    sti = i;
-                    stj = j;
+        
+        if (grid == null || grid.length == 0){
+            return 0;
+        }
+            
+        int m = grid.length;
+        int n = grid[0].length;
+        int countZero = 1;   
+        //loop to count 0's in the matrix
+        for(int currentRow = 0; currentRow < m; currentRow++){
+            for(int currentCol = 0; currentCol < n; currentCol++){
+                if(grid[currentRow][currentCol] == 0){
+                    countZero++;
                 }
             }
         }
-        int total = (n * m) - block;
-        boolean[][]vis = new boolean[n][m];
-        helper(grid, sti, stj, vis, new ArrayList<>(), total);
-        return paths.size();
+        
+        //loop to find value 1 in the matrix and then make a call to function totalWays
+        for(int currentRow = 0; currentRow < m; currentRow++){
+            for(int currentCol = 0; currentCol < n; currentCol++){
+                if(grid[currentRow][currentCol] == 1){
+                    totalWays(grid, currentRow, currentCol, m, n, countZero);
+                }
+            }
+        }
+        
+        return count; 
     }
+    
+    private int totalWays(int[][] grid, int currentRow, int currentCol, int m, int n, int countZero){
+        
+        if(currentRow >= m || currentCol >= n || currentRow < 0 || currentCol < 0 || grid[currentRow][currentCol] == -1 || grid[currentRow][currentCol] == -2){
+            return 0;
+        }
+        
+        if(grid[currentRow][currentCol] == 2 && countZero == 0){
+            count++;
+            return count;
+        }
+        
+    
+        int temp = grid[currentRow][currentCol];
+        grid[currentRow][currentCol] = -2;
+        
+        int right = totalWays(grid, currentRow, currentCol+1, m, n, countZero-1);
+        int left = totalWays(grid, currentRow, currentCol-1, m, n, countZero-1);
+        int up = totalWays(grid, currentRow-1, currentCol, m, n, countZero-1);
+        int down = totalWays(grid, currentRow+1, currentCol, m, n, countZero-1);
+        
+        grid[currentRow][currentCol] = temp;
+        return (right+left+up+down);
+        
+    }
+    
 }
