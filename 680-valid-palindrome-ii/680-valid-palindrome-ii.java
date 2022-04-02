ddd495df9256f19e0cf3591ef14baf23312c6efd
Aman Gupta <aamangptaa@gmail.com>
2022-04-02 16:04:01 +0530

    Time: 10 ms (59.94%), Space: 54.7 MB (35.26%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -1,24 +1,17 @@
 class Solution {
-    public boolean help(int i, int j, String s){
-        while(i <= j){
-            if(s.charAt(i) != s.charAt(j)) return false;
-            i++;
-            j--;
-        }return true;
-    }
     public boolean validPalindrome(String s) {
-        int n = s.length(), j = n - 1, i = 0;
-        boolean flag = false;
-        while(i <= j){
-            int ci = s.charAt(i), cj = s.charAt(j);
-            if(ci != cj && flag == false){
-                flag = true;
-                return help(i + 1, j,s) || help(i, j - 1, s);
-            }else if(ci != cj && flag == true) return false;
-            else {
-                i++;j--;
-            }
-        }
-        return true;
+        if(s == null || s.length() == 0) return false;
+		
+        int i = 0, j = s.length()-1;
+        while(i < j && s.charAt(i) == s.charAt(j)) {i++; j--;}
+        if(i >= j) return true;
+
+        int i1 = i, j1 = j-1; 
+        while(i1 < j1 && s.charAt(i1) == s.charAt(j1)) {i1++; j1--;}
+        if(i1 >= j1) return true;
+
+        int i2 = i+1, j2 = j;
+        while(i2 < j2 && s.charAt(i2) == s.charAt(j2)) {i2++; j2--;}
+        return i2 >= j2;
     }
 }
