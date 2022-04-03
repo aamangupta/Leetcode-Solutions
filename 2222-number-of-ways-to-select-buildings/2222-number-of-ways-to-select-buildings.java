3c91f130f41721a973dc157273e05f6c319d9316
Aman Gupta <aamangptaa@gmail.com>
2022-04-03 16:17:18 +0530

    Time: 39 ms (100.00%), Space: 42.3 MB (100.00%) - LeetHub

--- 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.java
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.java
@@ -1,5 +1,11 @@
 class Solution {
     public long numberOfWays(String s) {
+        
+        // there are only 2 combinations :
+        // 010 and 101
+        //     (noc = no. of combinations)
+        //     in 010 : when we get s.charAT(i) == '1' then noc = ans + no.of 0 before i * no.of 0 after i
+        //     in 101 : when we get s.charAT(i) == '0' then noc = ans + no.of 1 before i * no.of 1 after i
         long ans = 0, n = s.length(), t0 = 0, t1 = 0, curr0 = 0, curr1 = 0;
         for(int i = 0; i < n; i++){
             t0 += (s.charAt(i) == '0'? 1 : 0);
