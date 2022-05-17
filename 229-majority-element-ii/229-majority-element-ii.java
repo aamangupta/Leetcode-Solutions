929f48cceac116f3a36be6f38aae3770cd1c3518
Aman Gupta <aamangptaa@gmail.com>
2022-05-18 00:54:33 +0530

    Time: 3 ms (74.03%), Space: 49.8 MB (43.50%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,16 +1,36 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Arrays.sort(nums);
+        
+        // Moore voting algo
+        // there can only be 2 numbers possible that appear more than n/3 
+        
         int n = nums.length, c = 1;
         List<Integer> list = new ArrayList<>();
-        for(int i = 1; i < n; i++){
-            if(nums[i] == nums[i - 1]) c++;
-            else{
-                if(c > n/3) list.add(nums[i - 1]);
-                c = 1;
+        int c1 = 0, c2 = 0, val1 = -1, val2 = -1;
+        
+        for(int i : nums){
+            if(i == val1) c1++;
+            else if(i == val2) c2++;
+            else if(c1 == 0){
+                val1 = i;
+                c1 = 1;
+            }else if(c2 == 0){
+                val2 = i;
+                c2 = 1;
+            }else{
+                c1--;
+                c2--;
             }
         }
-        if(c > n/3) list.add(nums[n - 1]);
+        c1 = 0;
+        c2 = 0;
+        for(int i : nums){
+            if(i == val1) c1++;
+            if(i == val2) c2++;
+        }
+        if(c1 > n/3) list.add(val1);
+        if(val1 == val2) return list;
+        if(c2 > n/3) list.add(val2);
         return list;
     }
 }
