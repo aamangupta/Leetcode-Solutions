564893570f682a3bd2b6e6d337bd5c93ac78b8ec
Aman Gupta <aamangptaa@gmail.com>
2022-05-20 18:04:50 +0530

    Time: 1 ms (52.25%), Space: 42.4 MB (41.74%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,34 +1,22 @@
 class Solution {
-    int ans = 0;
-    public int helper(int [][]obs, int i, int j, boolean[][] vis, int [][]dir, int[][]dp){
-        int n = obs.length, m = obs[0].length;
-        if(i == n - 1 && j == m - 1) {
-            return 1;
-        }
-        if(dp[i][j] != 0){
-            return dp[i][j];    
-        }
-        vis[i][j] = true;
-        int val = 0;
-        for(int a = 0; a < 2; a++){
-            int ni = dir[a][0] + i;
-            int nj = dir[a][1] + j;
-            
-            if(ni >= 0 && ni < n && nj >= 0 && nj < m && vis[ni][nj] == false && obs[ni][nj] != 1){
-                val += helper(obs, ni, nj, vis, dir, dp);
+    public int uniquePathsWithObstacles(int[][] arr) {
+        int n = arr.length, m = arr[0].length, dp[][] = new int[n][m];
+        boolean [][]vis = new boolean[n][m];
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = m - 1; j >= 0; j--){
+                if(i == n - 1 && j == m - 1 && arr[i][j] != 1){
+                    dp[i][j] = 1;
+                }else if(i == n - 1 && arr[i][j] != 1){
+                    dp[i][j] = dp[i][j + 1];
+                }else if(j == m - 1 && arr[i][j] != 1){
+                    dp[i][j] = dp[i + 1][j];
+                }else{
+                    if(arr[i][j] != 1){
+                        dp[i][j] += dp[i + 1][j] + dp[i][j + 1];
+                    }
+                }
             }
         }
-        vis[i][j] = false;
-        dp[i][j] = val;
-        return val;
-    }
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int n = obstacleGrid.length, m = obstacleGrid[0].length;
-        if(n == 1 && m == 1) return obstacleGrid[0][0] != 1 ? 1 : 0;
-        if(obstacleGrid[0][0] == 1 || obstacleGrid[n - 1][m - 1] == 1) return 0;
-        boolean [][]vis = new boolean[n][m];
-        int dir[][] = {{0,1},{1,0}};
-        int [][]dp = new int[n][m];
-        return helper(obstacleGrid, 0, 0, vis, dir, dp);
+        return dp[0][0];
     }
 }
