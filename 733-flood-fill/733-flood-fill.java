c7c8bb42cfb95923f33aa92b43e5519ac14482fb
Aman Gupta <aamangptaa@gmail.com>
2022-05-25 23:43:25 +0530

    Time: 1 ms (82.72%), Space: 48.1 MB (37.56%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -6,25 +6,22 @@ class Solution {
             this.b = b;
         }
     }
-    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        Queue<pair> q = new ArrayDeque<>();
-        
-        int dir[] = {0,1,0,-1,0}, n = image.length, m = image[0].length, color = image[sr][sc];
-        if(color == newColor) return image; 
-        
-        q.add(new pair(sr, sc));
-        while(q.size() > 0) {
-            pair rem = q.remove();
-            int i = rem.a, j = rem.b;
-            image[i][j] = newColor;
-            
-            for(int k = 0; k < 4; k++){
-                int ni = i + dir[k], nj = j + dir[k + 1];
-                if(ni >= 0 && nj >= 0 && ni < n && nj < m && image[ni][nj] == color){
-                    q.add(new pair(ni, nj));
+    public void helper(int [][]arr, int sr, int sc, int ncolor){
+        int n = arr.length, m = arr[0].length, color = arr[sr][sc];
+        arr[sr][sc] = ncolor;
+        for(int k = 0; k < 4; k++){
+                int ni = sr + dir[k], nj = sc + dir[k + 1];
+                if(ni >= 0 && nj >= 0 && ni < n && nj < m && arr[ni][nj] == color)                 {
+                    helper(arr, ni, nj, ncolor);
                 }
             }
-        }
+    }
+    int dir[] = {0,1,0,-1,0};
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        
+        int n = image.length, m = image[0].length, color = image[sr][sc];
+        if(color == newColor) return image; 
+        helper(image, sr, sc, newColor);
         return image;
     }
 }
