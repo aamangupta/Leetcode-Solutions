0fd98db693aa16308946d9d26c95386385cf98a5
Aman Gupta <aamangptaa@gmail.com>
2022-05-29 00:31:31 +0530

    Time: 19 ms (70.92%), Space: 60.9 MB (53.99%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -1,10 +1,9 @@
 class Solution {
     public class pair{
-        int i , j , val;
-        pair(int i, int j, int val){
+        int i , j;
+        pair(int i, int j){
             this.i = i;
             this.j = j;
-            this.val = val;
         }
     }
     public int maxDistance(int[][] grid) {
@@ -12,22 +11,22 @@ class Solution {
         Queue<pair> q = new ArrayDeque<>();
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                if(grid[i][j] == 1) q.add(new pair(i,j,0));
+                if(grid[i][j] == 1) q.add(new pair(i,j));
             }
         }
         while(q.size() > 0){
             // System.out.println("aksj");
             pair rem = q.remove();
-            int i = rem.i, j = rem.j, val = rem.val;
-            ans = Math.max(ans, val);
+            int i = rem.i, j = rem.j;
             for(int k = 0; k < 4; k++){
                 int ni = i + dir[k], nj = j + dir[k + 1];
                 if(ni >= 0 && nj >= 0 && ni < n && nj < n && grid[ni][nj] == 0){
-                    grid[ni][nj] = 1;
-                    q.add(new pair(ni, nj, val + 1));
+                    grid[ni][nj] = grid[i][j]+1;
+                    ans = Math.max(ans, grid[ni][nj]);
+                    q.add(new pair(ni, nj));
                 }
             }
         }
-        return ans == 0 ? -1 : ans;
+        return ans-1;
     }
 }
