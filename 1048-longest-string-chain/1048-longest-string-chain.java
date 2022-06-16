f23c6af5e2c733141b1be6e0cc3896cabe02c567
Aman Gupta <aamangptaa@gmail.com>
2022-06-16 16:14:54 +0530

    Time: 961 ms (5.02%), Space: 144.4 MB (5.40%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -24,43 +24,17 @@ class Solution {
         
         int n = words.length, ans = 1, dp[] = new int[n];
         
-        HashMap<Integer, HashSet<String>> map = new HashMap<>();
-        for(String s : words){
-            HashSet<String> set = new HashSet<>();
-            int l = s.length();
-            if(map.containsKey(l) == false){
-                set.add(s);
-            }else{
-                set = map.get(l);
-                set.add(s);
-            }
-            map.put(l, set);
-        }
         Map<String, Integer> ref = new HashMap<>();
         for(String s : words) ref.put(s, 1);
         for(String s : words){
             int l = s.length();
-            if(map.containsKey(l - 1)){
-                HashSet<String> innerSet = map.get(l-1);
-                for(String s1 : innerSet){
-                    if(isPre(s, s1)){
-                        ref.put(s, Math.max(ref.get(s1) + 1, ref.get(s)));
-                    }
+            for(String s1 : ref.keySet()){
+                if(isPre(s, s1)){
+                    ref.put(s, Math.max(ref.get(s1) + 1, ref.get(s)));
                 }
             }
             ans = Math.max(ref.get(s), ans);
         }
         return ans;
-        // Arrays.fill(dp, 1);
-        // for(int i = 1; i < n; i++){
-        //     String str = words[i];
-        //     for(int j = 0; j < i; j++){
-        //         if(isPre(str, words[j])){
-        //             dp[i] = Math.max(dp[i], 1 + dp[j]);
-        //         }
-        //     }
-        //     ans = Math.max(dp[i], ans);
-        // }
-        // return ans;
     }
 }
