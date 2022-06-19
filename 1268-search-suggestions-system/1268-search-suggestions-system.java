1bacf358560060fb384a12b22445e7cb0f8467d5
Aman Gupta <aamangptaa@gmail.com>
2022-06-20 00:53:21 +0530

    Time: 34 ms (71.48%), Space: 46 MB (91.12%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,8 +1,16 @@
 class Solution {
-    public boolean find(String s1, String s2){
-        if(s2.length() < s1.length()) return false;
-        for(int i = 0; i < s1.length(); i++){
-            if(s2.charAt(i) != s1.charAt(i)) return false;
+    // -Sorting the array(we want lexicographically ans)
+    // -travelling through all the substings of the searhword string 
+    // -now comparing in the prod array for all the ele in it with if that prefix 
+    //  is present in that ele or not.
+    //     also limiting it till 3 string 
+    // -adding in the ans AL
+    
+    
+    public boolean find(String pref, String str){
+        if(str.length() < pref.length()) return false;
+        for(int i = 0; i < pref.length(); i++){
+            if(str.charAt(i) != pref.charAt(i)) return false;
         }
         return true;
     }
