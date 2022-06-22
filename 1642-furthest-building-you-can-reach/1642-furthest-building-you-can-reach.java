7762a2a4e2fcdbfe334e2f27f43631e604620ec8
Aman Gupta <aamangptaa@gmail.com>
2022-06-22 16:55:06 +0530

    Time: 43 ms (16.61%), Space: 79.5 MB (28.51%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.java
@@ -1,24 +1,36 @@
 class Solution {
+    
+    // the idea is using using bricks and then if more diff. is there then using ladder
+    // decreasing diff from bricks and also storing the diff in priorityqueue
+    // whenever bricks < 0
+    // brick += pq.remove();
+    //    then chekcing if ladder exist 
+            // if yes, then dec. ladder
+            // else return ans
+    
+    
     public int furthestBuilding(int[] heights, int bricks, int ladders) {
         int ans = 0, i = 1, n = heights.length;
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         pq.add(0);
         while(i < n){
             if(heights[i] <= heights[i - 1]){
-                i++;
                 ans++;
             }else{
                 int reqHeight = heights[i] - heights[i - 1];
                 bricks -= reqHeight;
                 pq.add(reqHeight);
-                ans++;
-                i++;
+                
                 if(bricks < 0){
                     bricks += pq.remove();
-                    if(ladders >= 1) ladders--;
-                    else return --ans;
-                }
+                    if(ladders >= 1){
+                        ladders--;
+                        ans++;    
+                    } 
+                    else return ans;
+                }else ans++;
             }
+            i++;
         }
         return --i;
     }
