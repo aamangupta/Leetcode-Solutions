8bcf33940f8c8a23d0844baacd286ffdb0bf9c86
Aman Gupta <aamangptaa@gmail.com>
2022-07-02 00:07:46 +0530

    Time: 28 ms (11.58%), Space: 50.7 MB (59.86%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
@@ -1,4 +1,8 @@
 class Solution {
+    // sorting array acc. to the no. of units in desc. order
+    // now incrementing the count of trucks and units acc to the condition
+    // prefering more no. of trucks from starting of arr.
+    
     public int maximumUnits(int[][] boxTypes, int truckSize) {
         Arrays.sort(boxTypes, (a,b) -> {
             return b[1] - a[1];
