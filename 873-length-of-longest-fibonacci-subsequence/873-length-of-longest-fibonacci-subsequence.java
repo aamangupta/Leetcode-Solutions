6a063c01f3fa41e07141fcd5229f08a8468b599c
Aman Gupta <aamangptaa@gmail.com>
2022-07-06 19:20:39 +0530

    Time: 469 ms (12.85%), Space: 117.2 MB (57.83%) - LeetHub

---
+++ 873-length-of-longest-fibonacci-subsequence/873-length-of-longest-fibonacci-subsequence.java
@@ -0,0 +1,41 @@
+class Solution {
+    // checking all the elements from arr[0] to arr[n]
+    // initially, with first ele, a = arr[i] and b = arr[j]
+    // loop till set contains(a + b) and update a = b and b = sum;
+    // update the answer
+    
+    public int lenLongestFibSubseq(int[] A) {
+        Set<Integer> s = new HashSet<Integer>();
+        for (int x : A) s.add(x);
+        int res = 2;
+        for (int i = 0; i < A.length; ++i)
+            for (int j = i + 1; j < A.length; ++j) {
+                int a = A[i], b = A[j], l = 2;
+                while (s.contains(a + b)) {
+                    b = a + b;
+                    a = b - a;
+                    l++;
+                }
+                res = Math.max(res, l);
+            }
+        return res > 2 ? res : 0;
+//         HashSet<Integer> set = new HashSet<>();
+//         int n = arr.length, ans = 1;
+//         for(int i : arr) set.add(i);
+        
+//         for(int i = 0; i < n; i++){
+//             int a = arr[i], pot = 1;
+//             for(int j = i + 1; j < n; j++){
+//                 int b = arr[j], count = 2;
+//                 while(set.contains(a + b)){
+//                     int sum = a + b;
+//                     a = b;
+//                     b = sum;
+//                     count++;
+//                 }
+//                 ans = Math.max(ans, count);
+//             }
+//         }
+//         return ans;
+    }
+}
