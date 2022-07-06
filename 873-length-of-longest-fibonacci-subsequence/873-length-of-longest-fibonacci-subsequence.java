97b9201bb01a931a1953b91dc47abd03817c0601
Aman Gupta <aamangptaa@gmail.com>
2022-07-06 19:24:26 +0530

    Time: 400 ms (25.70%), Space: 117.4 MB (51.00%) - LeetHub

--- 873-length-of-longest-fibonacci-subsequence/873-length-of-longest-fibonacci-subsequence.java
+++ 873-length-of-longest-fibonacci-subsequence/873-length-of-longest-fibonacci-subsequence.java
@@ -4,38 +4,37 @@ class Solution {
     // loop till set contains(a + b) and update a = b and b = sum;
     // update the answer
     
-    public int lenLongestFibSubseq(int[] A) {
-        Set<Integer> s = new HashSet<Integer>();
-        for (int x : A) s.add(x);
-        int res = 2;
-        for (int i = 0; i < A.length; ++i)
-            for (int j = i + 1; j < A.length; ++j) {
-                int a = A[i], b = A[j], l = 2;
-                while (s.contains(a + b)) {
-                    b = a + b;
-                    a = b - a;
-                    l++;
-                }
-                res = Math.max(res, l);
-            }
-        return res > 2 ? res : 0;
-//         HashSet<Integer> set = new HashSet<>();
-//         int n = arr.length, ans = 1;
-//         for(int i : arr) set.add(i);
+    public int lenLongestFibSubseq(int[] arr) {
+        // Set<Integer> s = new HashSet<Integer>();
+        // for (int x : A) s.add(x);
+        // int res = 2;
+        // for (int i = 0; i < A.length; ++i)
+        //     for (int j = i + 1; j < A.length; ++j) {
+        //         int a = A[i], b = A[j], l = 2;
+        //         while (s.contains(a + b)) {
+        //             b = a + b;
+        //             a = b - a;
+        //             l++;
+        //         }
+        //         res = Math.max(res, l);
+        //     }
+        // return res > 2 ? res : 0;
+        HashSet<Integer> set = new HashSet<>();
+        int n = arr.length, ans = 2;
+        for(int i : arr) set.add(i);
         
-//         for(int i = 0; i < n; i++){
-//             int a = arr[i], pot = 1;
-//             for(int j = i + 1; j < n; j++){
-//                 int b = arr[j], count = 2;
-//                 while(set.contains(a + b)){
-//                     int sum = a + b;
-//                     a = b;
-//                     b = sum;
-//                     count++;
-//                 }
-//                 ans = Math.max(ans, count);
-//             }
-//         }
-//         return ans;
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                int a = arr[i], b = arr[j], count = 2;
+                while(set.contains(a + b)){
+                    int sum = a + b;
+                    a = b;
+                    b = sum;
+                    count++;
+                }
+                ans = Math.max(ans, count);
+            }
+        }
+        return ans > 2 ? ans : 0;
     }
 }
